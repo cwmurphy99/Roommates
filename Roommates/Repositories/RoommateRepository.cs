5ba23f50c5eda4fa35e7ffc9b7006fd98f5ace41
Chris Murphy <theworldisyours@gmail.com>
2021-12-06 17:03:24 -0500

    first commit

---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roommates.Models;
+using Roommates.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT FirstName, RentPortion, r.Name
+                                        FROM Roommate rm
+                                        join Room r on r.Id = rm.RoomId
+                                        WHERE rm.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+                        if(reader.Read())
+                        {
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("firstName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion"))
+                            };
+                        }
+                        return roommate;
+                    }
+                }
+            }
+        }
+       
+    }
+}
